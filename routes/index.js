0b4d78f3b44c9e09443d45b93a4d3c9359b978f6
MiceLikeRice <3014711281@qq.com>
2023-11-01 09:28:00 +0800

    desc

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router=express.Router();
 router.use("/user",require("./user"));
 router.use("/blog",require("./blog"));
 router.use("/image",require("./image"));
-
+router.use("/resource",require("./resource"))
 
 
 module.exports=router;
